1eeacb23aebee72f494b80f0dcd952b8e8c4a4b0
Luke Chadwick <luke.a.chadwick@gmail.com>
2013-01-01 10:03:50 +1100

    Try again to get it logging to newrelic

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,3 +10,7 @@ before_fork do |server, worker|
   @resque_pid ||= spawn("bundle exec rake resque:work QUEUES=*")
   @scheduler_pid ||= spawn("bundle exec rake resque:scheduler")
 end
+
+# after_fork do |server, worker|
+#   NewRelic::Agent.manual_start :app_name => "Windchill (#{ENV["RACK_ENV"]})"
+# end
